36dc1ebb44c85b353460eacabbb9711a5ddeefb1
xuranechen <36075550+xuranechen@users.noreply.github.com>
2025-02-07 17:36:53 +0800

    过滤无用ip

--- Scripts/select_ip.gd
+++ Scripts/select_ip.gd
@@ -1,27 +1,27 @@
 extends "res://Scripts/config_base.gd"
 
 
-var ip_list:OptionButton
+var ip_list: OptionButton
 
 func _ready() -> void:
 	ip_list = get_node("/root/Node2D/Control/VBoxContainer/ip/IPList")
-	
+
 	_get_simple_network_info()
-	
+
 func _get_simple_network_info():
 	var interfaces = IP.get_local_interfaces()
 	for interface in interfaces:
 		var interface_name = interface["friendly"]
 		var addresses = interface["addresses"]
 		var interface_type = _get_interface_type(interface_name)
-		
+
 		# 打印接口的所有内容
 		print("Interface: %s" % [interface_name])
 		print("  Type: %s" % [interface_type])
 		print("  Addresses:")
 		for address in addresses:
-			if address != "127.0.0.1":
-				ip_list.add_item(address)
+			if interface_type != "Unknown" and address != "127.0.0.1" and ":" not in address:
+				ip_list.add_item(interface_type + ":  " + address)
 				print("    %s" % [address])
 
 # 根据接口名称判断类型
