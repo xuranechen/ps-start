083e662986e9098459ef3874344823f9f23548bd
xurane <rune_chen@163.com>
2025-02-07 21:27:29 +0800

    选择ip

--- Scripts/select_ip.gd
+++ Scripts/select_ip.gd
@@ -1,12 +1,22 @@
 extends "res://Scripts/config_base.gd"
 
-
 var ip_list: OptionButton
+var ip_input: LineEdit
 
 func _ready() -> void:
 	ip_list = get_node("/root/Node2D/Control/VBoxContainer/ip/IPList")
-
+	ip_input = get_node("/root/Node2D/Control/VBoxContainer/ip/IP")
+	
+	ip_list.item_selected.connect(_set_ip)
+	ip_input.text = _read_config("SelectedIP")
 	_get_simple_network_info()
+	
+	# 查找选项字符串中是否包含_read_config("SelectedIP")，如果包含则设置选择该选项
+	var selected_ip = _read_config("SelectedIP")
+	for i in range(ip_list.get_item_count()):
+		if ip_list.get_item_text(i).find(selected_ip) != -1:
+			ip_list.select(i)
+			break
 
 func _get_simple_network_info():
 	var interfaces = IP.get_local_interfaces()
@@ -15,7 +25,6 @@ func _get_simple_network_info():
 		var addresses = interface["addresses"]
 		var interface_type = _get_interface_type(interface_name)
 
-		# 打印接口的所有内容
 		print("Interface: %s" % [interface_name])
 		print("  Type: %s" % [interface_type])
 		print("  Addresses:")
@@ -32,3 +41,7 @@ func _get_interface_type(interface_name: String) -> String:
 		return "以太网"
 	else:
 		return "Unknown"
+		
+func _set_ip(index: int):
+	ip_input.text = ip_list.get_item_text(index).split(":")[1]
+	_save_config("SelectedIP", ip_input.text)
